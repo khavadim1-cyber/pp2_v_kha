1b0b819efa224cb36c2b54ef850ebece2a251f5b
Vadim Kha <vadimkha@MacBook-Air-Vadim.local>
2026-01-27 11:29:28 +0500

    first commit

---
+++ practise2/boolean/boolean_intro.py
@@ -0,0 +1,14 @@
+print(10 > 9)#example1
+print(10 == 9)
+print(10 < 9)
+
+a = 200#example2
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))#example3
+print(bool(15))
+
