f6644b56e72dca39d7e2e2c9d5f7440a1fcdbb1b
Vadim Kha <vadimkha@192.168.10.5>
2026-02-21 21:26:33 +0500

    add exersices

--- practise4/generators.py
+++ practise4/generators.py
@@ -1,4 +1,4 @@
-'''def square(n):
+def square(n):#ex1
     for i in range(1, n+1):
         yield i * i
 n = int(input())
@@ -6,25 +6,24 @@ n = int(input())
 for i in square(n):
     print(i)
 
-n = int(input())
+n = int(input())#ex2
 print(*(i for i in range(0, n + 1, 2)), sep=",")
 
-def devisible(n):
+def devisible(n):#ex3
     for i in range(n + 1):
         if i % 3 == 0 and i%4 ==0:
             yield i
 n=int(input())
 for i in devisible(n):
-    print(i)'''
-
-def squares(a,b):
-    for i in range(a, b+1):
-        yield i * i
-a = int(input())
-b= int(input())
-
-for i in squares(a,b):
     print(i)
 
-n=int(input())
+def squares(a,b):#ex4
+    for i in range(a, b+1):
+        yield i * i
+a=list(map(int,input().split()))
+for i in squares(a[0],a[1]):
+    print(i)
+
+n=int(input())#ex5
 print(*(i for i in range(n, -1, -1)), sep=" ")
+
