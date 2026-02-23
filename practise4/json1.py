87735ca7b0ae996f105ae3f8f0cef2a202d77f3a
Vadim Kha <vadimkha@192.168.10.5>
2026-02-24 01:16:03 +0500

    new solution

--- practise4/json1.py
+++ practise4/json1.py
@@ -1,21 +1,14 @@
 import json
-
-
 with open("sample-data.json") as f:
-    data = json.load(f)
+    text = f.read()
+
+data = json.loads(text)
 
 print("Interface Status")
-print("=" * 80)
-print(f"{'DN':50} {'Description':20} {'Speed':7} {'MTU':5}")
-print("-" * 80)
-
+print("="*80)
+print(f"{'DN':50} {'Description':20} {'Speed':8} {'MTU':5}")
+print("-"*80)
 
 for item in data["imdata"]:
-    attrs = item["l1PhysIf"]["attributes"]
-
-    dn = attrs.get("dn", "")
-    descr = attrs.get("descr", "")
-    speed = attrs.get("speed", "")
-    mtu = attrs.get("mtu", "")
-
-    print(f"{dn:50} {descr:20} {speed:7} {mtu:5}")
+    a = item["l1PhysIf"]["attributes"]
+    print(f"{a['dn']:50} {a['descr']:20} {a['speed']:8} {a['mtu']}")
